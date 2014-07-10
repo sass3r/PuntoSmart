c5ea0b2bf07258643ab03ba2ffdc85dac8c07496
sass3r <mrsblast@gmail.com>
2014-07-10 12:07:10 -0400

    repositorio actualizado \m/

--- cubitrucktest/src/com/sawers/cubitrucktest/MainActivity.java
+++ cubitrucktest/src/com/sawers/cubitrucktest/MainActivity.java
@@ -6,10 +6,12 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.view.View;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.File;
 
 
@@ -20,10 +22,13 @@ public class MainActivity extends Activity
     private ScrollView sv;
     private LinearLayout linearLayout;
     private TextView tv;
-    private Button boton, boton1;
+    private EditText entry;
+    private Button boton, boton1, boton2;
     public SerialPort serialPort;
     private InputStream input;
     private OutputStream output;
+    private OutputStreamWriter writer;
+
 
     public void onCreate(Bundle savedInstanceState)
     {
@@ -35,20 +40,24 @@ public class MainActivity extends Activity
     }
 
     public void initGui(){
+      entry = new EditText(this);
       tv = new TextView(this);
       boton = new Button(this);
       boton.setText("Buscar HWSerial");
       boton1 = new Button(this);
       boton1.setText("Open Predefined Serial");
+      boton2 = new Button(this);
+      boton2.setText("Llamar");
       tv.setText("Bienvenido");
       sv = new ScrollView(this);
       linearLayout = new LinearLayout(this);
       linearLayout.setOrientation(LinearLayout.VERTICAL);
       sv.addView(linearLayout);
+      linearLayout.addView(entry);
       linearLayout.addView(tv);
       linearLayout.addView(boton);
       linearLayout.addView(boton1);
-
+      linearLayout.addView(boton2);
     }
 
     public void actions(){
@@ -63,6 +72,12 @@ public class MainActivity extends Activity
       boton1.setOnClickListener(new View.OnClickListener(){
           public void onClick(View v){
             conectarSerial();
+          }
+      });
+
+      boton2.setOnClickListener(new View.OnClickListener(){
+          public void onClick(View v){
+            llamar();
           }
       });
     }
@@ -89,5 +104,19 @@ public class MainActivity extends Activity
 
         tv.setText("Conectado a /dev/ttyS0");
 
+    }
+
+    public void llamar(){
+      String num = entry.getText().toString();
+      String cmd = "";
+      writer = new OutputStreamWriter(output);
+      cmd = "ATD" + num + ";" + "\r\n";
+      try{
+        writer.write(cmd,0,cmd.length());
+        writer.flush();
+
+      }catch(IOException ioe){
+
+      }
     }
 }
--- rollback/MainActivity.java
+++ rollback/MainActivity.java
@@ -6,10 +6,12 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.view.View;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.File;
 
 
@@ -20,35 +22,16 @@ public class MainActivity extends Activity
     private ScrollView sv;
     private LinearLayout linearLayout;
     private TextView tv;
-    private Button boton, boton1, boton2, boton3, boton4, boton5, boton6, write;
+    private EditText entry;
+    private Button boton, boton1, boton2;
     public SerialPort serialPort;
-    public SerialPort serialPort1;
-    public SerialPort serialPort2;
-    public SerialPort serialPort3;
-    public SerialPort serialPort4;
-    public SerialPort serialPort5;
-    public InputStream input;
-    public OutputStream output;
-    public InputStream input1;
-    public OutputStream output1;
-    public InputStream input2;
-    public OutputStream output2;
-    public InputStream input3;
-    public OutputStream output3;
-    public InputStream input4;
-    public OutputStream output4;
-    public InputStream input5;
-    public OutputStream output5;
-
-
-
-    String res;
+    private InputStream input;
+    private OutputStream output;
+    private OutputStreamWriter writer;
 
 
     public void onCreate(Bundle savedInstanceState)
     {
-        res = "";
-
         super.onCreate(savedInstanceState);
         initGui();
         serialFinder = new SerialPortFinder();
@@ -57,38 +40,24 @@ public class MainActivity extends Activity
     }
 
     public void initGui(){
+      entry = new EditText(this);
       tv = new TextView(this);
       boton = new Button(this);
       boton.setText("Buscar HWSerial");
       boton1 = new Button(this);
-      boton1.setText("Open /dev/ttyGS3");
-      tv.setText("Bienvenido");
+      boton1.setText("Open Predefined Serial");
       boton2 = new Button(this);
-      boton2.setText("Open /dev/ttyGS2");
-      boton3 = new Button(this);
-      boton3.setText("Open /dev/ttyGS1");
-      boton4 = new Button(this);
-      boton4.setText("Open /dev/ttyGS0");
-      boton5 = new Button(this);
-      boton5.setText("Open /dev/ttyS2");
-      boton6 = new Button(this);
-      boton6.setText("Open /dev/ttyS0");
-      write = new Button(this);
-      write.setText("Write");
-
+      boton2.setText("Llamar");
+      tv.setText("Bienvenido");
       sv = new ScrollView(this);
       linearLayout = new LinearLayout(this);
       linearLayout.setOrientation(LinearLayout.VERTICAL);
       sv.addView(linearLayout);
+      linearLayout.addView(entry);
       linearLayout.addView(tv);
       linearLayout.addView(boton);
       linearLayout.addView(boton1);
       linearLayout.addView(boton2);
-      linearLayout.addView(boton3);
-      linearLayout.addView(boton4);
-      linearLayout.addView(boton5);
-      linearLayout.addView(boton6);
-      linearLayout.addView(write);
     }
 
     public void actions(){
@@ -102,79 +71,14 @@ public class MainActivity extends Activity
 
       boton1.setOnClickListener(new View.OnClickListener(){
           public void onClick(View v){
-            conectarSerial("/dev/ttyGS3", serialPort, input, output);
-            if(input != null && output != null ){
-              boton1.setText("Conected");
-            }else{
-              boton1.setText("Ugly");
-            }
+            conectarSerial();
           }
       });
 
       boton2.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-          conectarSerial("/dev/ttyGS2", serialPort1, input1, output1);
-          if(input1 != null && output1 != null ){
-            boton2.setText("Conected");
-          }else{
-            boton2.setText("Ugly");
+          public void onClick(View v){
+            llamar();
           }
-        }
-      });
-
-      boton3.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-          conectarSerial("/dev/ttyGS1", serialPort2, input2, output2);
-          if(input2 != null && output2 != null ){
-            boton3.setText("Conected");
-          }else{
-            boton3.setText("Ugly");
-          }
-        }
-      });
-
-      boton4.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-          conectarSerial("/dev/ttyGS0", serialPort3, input3, output3);
-          if(input3 != null && output3 != null ){
-            boton4.setText("Conected");
-          }else{
-            boton4.setText("Ugly");
-          }
-        }
-      });
-
-      boton5.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-          conectarSerial("/dev/ttyS2", serialPort4, input4, output4);
-          if(input4 != null && output4 != null ){
-            boton5.setText("Conected");
-          }else{
-            boton5.setText("Ugly");
-          }
-        }
-      });
-
-      boton6.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-          conectarSerial("/dev/ttyS0", serialPort5, input5, output5);
-          if(input5 != null && output5 != null ){
-            boton6.setText("Conected");
-          }else{
-            boton6.setText("Ugly");
-          }
-        }
-      });
-
-      write.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-          escribirBasic(output5);/*
-          escribirBasic(output1);
-          escribirBasic(output2);
-          escribirBasic(output3);
-          escribirBasic(output4);
-          escribirBasic(output5);*/
-        }
       });
     }
 
@@ -187,25 +91,30 @@ public class MainActivity extends Activity
 
     }
 
-    private void conectarSerial(String ruta, SerialPort ser, InputStream in, OutputStream out){
-        File file = new File(ruta);
+    private void conectarSerial(){
+        File file = new File("/dev/ttyS0");
         try{
-          ser = new SerialPort(file,9600,0);
-          out = ser.getOutputStream();
-          in = ser.getInputStream();
+          serialPort = new SerialPort(file,9600,0);
+          output = serialPort.getOutputStream();
+          input = serialPort.getInputStream();
         }catch(IOException ioe){
 
 
         }
 
-        res = res + ruta + " ";
-        tv.setText(res);
+        tv.setText("Conectado a /dev/ttyS0");
 
     }
 
-    private void escribirBasic(OutputStream out){
+    public void llamar(){
+      String num = entry.getText().toString();
+      String cmd = "";
+      writer = new OutputStreamWriter(output);
+      cmd = "ATD" + num + ";" + "\r\n";
       try{
-        out.write(1);
+        writer.write(cmd,0,cmd.length());
+        writer.flush();
+
       }catch(IOException ioe){
 
       }
